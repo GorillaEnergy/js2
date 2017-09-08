896fc7ef3af663a1c904e4f70255fade6dbc15ba
GorillaEnergy <30631660+GorillaEnergy@users.noreply.github.com>
2017-09-08 15:02:12 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -490,11 +490,11 @@
     // ПОИСК
     search.addEventListener("input", function () {
         for (var i = 0; i<control.length; i++){
-            let blockContent = doc.getElementById(control[i]).innerText;
-            doc.getElementById(control[i]).style.display = '';
+            let blockContent = document.getElementById(i).innerText;
+            doc.getElementById(i).style.display = '';
             if (blockContent.toLowerCase().indexOf(search.value.toLowerCase()) === -1){
-                doc.getElementById(control[i]).style.display = 'none';
+                doc.getElementById(i).style.display = 'none';
             }
         }
     });
-console.log(control);
+//console.log(control);
